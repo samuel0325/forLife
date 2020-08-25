74c643720cfd5bf058a9de3f842f8c7d7593b570
JMC <fdop104@nate.com>
2020-08-26 01:11:55 +0900

    드디어 집에서 코딩하네1

--- tuple.py
+++ tuple.py
@@ -1,4 +1,12 @@
+#6475
 score = [(90,80),(85,75),(90,100)]
 for i in range(1,4):
     print("{}번 학생의 총점은 {}점이고, 평균은 {}입니다.".format(i, score[i-1][0]+score[i-1][1], (score[i-1][0]+score[i-1][1])/2))
-    
+
+#6275
+line = 'Python is powerful... and fast; plays well with others; runs everywhere; is friendly & easy to learn; is Open.'
+vowl = ['a','e','i','o','u']
+for i in vowl:
+    line = line.replace(i,"")
+print(line)
+
