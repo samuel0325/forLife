18f2e98abe9d50244a7e0a4225ddd9c08d40966b
JMC <fdop104@nate.com>
2020-09-03 03:39:58 +0900

    i want to do better!

--- miro.py
+++ miro.py
@@ -1,6 +1,70 @@
+# def IsSafe(y,x):
+#     return 0<=y<N and 0<=x<N and (Maze[y][x] == 0 or Maze[y][x] == 3)
+#
+# def DFS(start_y, start_x):
+#     global result
+#
+#     if Maze[start_y][start_x] == 3:
+#         result = 1
+#         return
+#
+#     visited.append((start_y, start_x))
+#     for dir in range(4):
+#         New_Y = start_y + dy[dir]
+#         New_X = start_x + dx[dir]
+#         if IsSafe(New_Y, New_X) and (New_Y, New_X) not in visited:
+#             DFS(New_Y, New_X)
+
 #4875 미로
-howmany = int(input())
-for i in range(1,howmany+1):
-    howbig = int(input())
-    map = [list(map(int,list(input()))) for _ in range(howbig)]
-    print(map)
+
+def Safe(y,x):
+    return 0<=x<N and 0<=y<N and (Maze[y][x]==0 or Maze[y][x]==3)
+
+def DFS(y,x):
+    global result
+
+    if Maze[y][x]==3:
+        result = 1
+        return
+
+    visited.append((y,x))
+    for dir in range(4):
+        new_X = x+dx[dir]
+        new_Y = y+dy[dir]
+        if (new_Y, new_X) not in visited and Safe(new_Y,new_X):
+            DFS(new_Y,new_X)
+
+TC = int(input())
+for tc in range(1, TC+1):
+    N = int(input())
+    Maze = [list(map(int, input())) for _ in range(N)]
+
+    for y in range(N):
+        for x in range(N):
+            if Maze[y][x] == 2:
+                start_y, start_x = y,x
+
+    #상, 하, 좌, 우
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+
+    visited = []
+    result = 0
+    DFS(start_y, start_x)
+    print('#%d %d'%(tc, result))
+
+# N = 3
+# Maze = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
+#
+# def IsSafe(y,x):
+#     return 0<=y<N and 0<=x<N and (Maze[y][x] == 0 or Maze[y][x] == 3)
+#
+# def DFS(start_y, start_x):
+#     global result
+#
+#     if Maze[start_y][start_x] == 3:
+#         result = 1
+#         return
+#
+# print(IsSafe(2,2))
+# print(DFS(2,2))
